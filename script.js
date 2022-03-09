419e4f9041baaa0f874bfd64f8798f1ed060fa7c
Felix <felix.schlegel@gmx.de>
2022-03-09 14:38:36 +0100

    refactor: made some smaller refactoring to be sonarlint compliant

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-var index = 0
-var links = []
-var people = []
-var ingredientCollection = []
+let index = 0;
+let links = [];
+let people = [];
+let ingredientCollection = [];
+
+const xhttp = new XMLHttpRequest();
 
 function getRecipes() {
-    var input = document.getElementById("links").value.split("\n")
+    links = []
+    people = []
+    ingredientCollection = []
 
-    if (input.length == 0 || input[0] == "")
-        return
+    const input = document.getElementById("links").value.split("\n");
 
-    for (let index = 0; index < input.length; index++) {
-        const element = input[index].split(";");
+    if (input.length === 0 || input[0] === "") return
 
-        if (element.length == 1)
-            console.log("Falsches Format bei Link " + index)
+    for (let i = 0; i < input.length; i++) {
+        const element = input[i].split(";");
+
+        if (element.length === 1) console.log("Falsches Format bei Link " + i)
         else {
             links.push(element[0].trim())
             people.push(element[1].trim())
         }
     }
 
-    if (links.length == 0)
-        return
+    if (links.length === 0) return
 
     xhttp.open("GET", links[0], true);
     xhttp.send();
 }
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var parser = new DOMParser()
-        var responseDoc = parser.parseFromString(xhttp.responseText, "text/html")
-
-        // var people = 4
+xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        const parser = new DOMParser();
+        const responseDoc = parser.parseFromString(xhttp.responseText, "text/html");
 
         ingredientCollection.push(getIngredients(responseDoc, people[index]))
 
@@ -48,9 +48,9 @@ xhttp.onreadystatechange = function() {
 };
 
 function getIngredients(responseDoc, people) {
-    var list = responseDoc.getElementsByClassName("col-md-offset-3")[2]
-   
-    var ingredients = []
+    const list = responseDoc.getElementsByClassName("col-md-offset-3")[2];
+
+    const ingredients = [];
 
     for (let i = 2; i < list.children.length; i++) {
         let row = list.children[i]
@@ -58,74 +58,63 @@ function getIngredients(responseDoc, people) {
         let amount = ""
         let unit = ""
         let name = ""
-        
-        if (row.childElementCount == 1) {
+
+        if (row.childElementCount === 1) {
             name = row.children[0].children[0].innerText.trim()
         } else {
-            amountUnitStrings = row.children[0].children[0].innerText.trim().split(" ")
+            const amountUnitStrings = row.children[0].children[0].innerText.trim().split(" ")
 
             amount = amountUnitStrings[0]
 
-            if (amountUnitStrings.length == 2)
-                unit = amountUnitStrings[1]
+            if (amountUnitStrings.length === 2) unit = amountUnitStrings[1]
 
-                name = row.children[1].children[0].innerText.trim()
+            name = row.children[1].children[0].innerText.trim()
         }
 
-        if (amount != "")
-            amount *= people
+        if (amount !== "") amount *= people
 
-        ingredients.push({ amount: amount, unit: unit, name: name })
+        ingredients.push({amount: amount, unit: unit, name: name})
     }
 
     return ingredients
 }
 
 function collectAllRecipes() {
-    if (ingredientCollection.length == 0)
-        return
+    if (ingredientCollection.length === 0) return
 
-    result = ingredientCollection[0]
+    console.log(ingredientCollection)
 
-    for (var i = 1; i < ingredientCollection.length; i++) {
-        for (let j = 0; j < ingredientCollection[i].length; j++) {
-            const element = ingredientCollection[i][j];
+    let result = ingredientCollection[0]
 
-            index = getIndex(element.name, element.unit)
+    for (const recipe of ingredientCollection) {
+        for (const ingredient of recipe) {
+            index = getIndex(ingredient.name, ingredient.unit, result)
 
-            if (index == -1)
-                result.push(element)
-            else
-                result[index].amount += element.amount
+            if (index === -1) result.push(ingredient)
+            else result[index].amount += ingredient.amount
         }
     }
 
     // Sort descending
-    result.sort(function(a, b) {return b.amount - a.amount})
+    result.sort(function (a, b) {
+        return b.amount - a.amount
+    })
 
-    // write everthing into textbox
-    var resultText = ""
-    for (let index = 0; index < result.length; index++) {
-        const element = result[index];
-
-        if (element.amount != "")
-            resultText += element.amount + (element.unit != "" ? "" : " ")
-        if (element.unit != "")
-            resultText += element.unit + " "
-        resultText += element.name
-        
-        if (index < result.length - 1)
-            resultText += "\n"
+    // write everything into textbox
+    let resultText = "";
+    for (const element of result) {
+        if (element.amount !== "") resultText += element.amount + (element.unit !== "" ? "" : " ")
+        if (element.unit !== "") resultText += element.unit + " "
+        resultText += element.name + "\n"
     }
 
     document.getElementById("ingredients").value = resultText
 }
 
-function getIndex(name, unit) {
-    for (let index = 0; index < result.length; index++) {
-        const element = result[index];
-        if (element.name == name && element.unit == unit)
-            return index
+function getIndex(name, unit, result) {
+    for (let i = 0; i < result.length; i++) {
+        const element = result[i];
+        if (element.name === name && element.unit === unit) return i
     }
     return -1
 }
