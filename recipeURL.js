ae74a9c0941ec3e72ced3cfebe0c2421fc2108e4
Felix <felix.schlegel@gmx.de>
2022-03-09 22:27:12 +0100

    feat: changed textarea element to single text inputs where the user can input a single url
    better user experience

---
+++ recipeURL.js
@@ -0,0 +1,3 @@
+function getIngredients(hostname, document) {
+
+}
--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 let index = 0;
-let links = [];
-let people = [];
+let recipes = [];
 let ingredientCollection = [];
+
+const supportedURLs = ["mobile.kptncook.com"]
+
+const urlForm = document.getElementById("urls").children[0].cloneNode(true)
 
 const xhttp = new XMLHttpRequest();
 
+function collectURLs() {
+    const forms = document.getElementById("urls").children
+
+    for (const form of forms) {
+        const url = form.getElementsByClassName("url")[0].value
+        const people = form.getElementsByClassName("people")[0].value
+
+        if (url === "" || people === "")
+            continue
+
+        // Check for valid inputs
+        if (!isURL(url) || isNaN(people) || people < 0) {
+            console.log(url + " and " + people + " are no valid input")
+            continue
+        }
+
+        // Check for supported urls
+        if (!isSupportedURL(url)) {
+            console.log("The URL " + url + " is not supported")
+            continue
+        }
+
+        recipes.push({url: url, people: people})
+    }
+}
+
 function getRecipes() {
-    links = []
-    people = []
+    recipes = []
     ingredientCollection = []
 
-    const input = document.getElementById("links").value.split("\n");
+    collectURLs()
 
-    if (input.length === 0 || input[0] === "") return
+    console.log(recipes)
 
-    for (let i = 0; i < input.length; i++) {
-        const element = input[i].split(";");
+    if (recipes.length === 0) return
 
-        if (element.length === 1) console.log("Falsches Format bei Link " + i)
-        else {
-            links.push(element[0].trim())
-            people.push(element[1].trim())
-        }
-    }
-
-    if (links.length === 0) return
-
-    xhttp.open("GET", links[0], true);
+    xhttp.open("GET", recipes[0].url, true);
     xhttp.send();
 }
 
@@ -35,14 +53,14 @@ xhttp.onreadystatechange = function () {
         const parser = new DOMParser();
         const responseDoc = parser.parseFromString(xhttp.responseText, "text/html");
 
-        ingredientCollection.push(getIngredients(responseDoc, people[index]))
+        ingredientCollection.push(getIngredients(responseDoc, recipes[index].people))
 
-        if (index + 1 < links.length) {
+        if (index + 1 < recipes.length) {
             index++
-            xhttp.open("GET", links[index], true);
+            xhttp.open("GET", recipes[index].url, true);
             xhttp.send();
         } else {
-            collectAllRecipes();
+            mergeAllRecipes();
         }
     }
 };
@@ -79,24 +97,23 @@ function getIngredients(responseDoc, people) {
     return ingredients
 }
 
-function collectAllRecipes() {
+function mergeAllRecipes() {
     if (ingredientCollection.length === 0) return
 
-    console.log(ingredientCollection)
-
-    let result = ingredientCollection[0]
-
+    let result = []
     for (const recipe of ingredientCollection) {
         for (const ingredient of recipe) {
             index = getIndex(ingredient.name, ingredient.unit, result)
 
-            if (index === -1) result.push(ingredient)
-            else result[index].amount += ingredient.amount
+            if (index === -1)
+                result.push(ingredient)
+            else
+                result[index].amount += ingredient.amount
         }
     }
 
     // Sort descending
-    result.sort(function (a, b) {
+    result.sort((a, b) => {
         return b.amount - a.amount
     })
 
@@ -112,9 +129,47 @@ function collectAllRecipes() {
 }
 
 function getIndex(name, unit, result) {
+    console.log(name)
+    console.log(result)
     for (let i = 0; i < result.length; i++) {
         const element = result[i];
-        if (element.name === name && element.unit === unit) return i
+        if (element.name === name && element.unit === unit) {
+            return i
+        }
     }
     return -1
+}
+
+function isSupportedURL(string) {
+    let url
+
+    try {
+        url = new URL(string)
+    } catch (_) {
+        return false
+    }
+
+    return supportedURLs.includes(url.hostname)
+}
+
+function isURL(string) {
+    let url
+
+    try {
+        url = new URL(string)
+    } catch (_) {
+        return false
+    }
+
+    console.log(url)
+
+    return url.protocol === "http:" || url.protocol === "https:"
+}
+
+function addURLForm() {
+    document.getElementById("urls").appendChild(urlForm.cloneNode(true))
+}
+
+function deleteURLForm(button) {
+    button.parentElement.parentElement.remove()
 }
